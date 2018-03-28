09b3ab221050136f3d23374a7f8b016e9c02de81
Mitch <beaumonm@amazon.com>
2018-03-29 07:38:12 +1100

    Updating detailed documentation

--- scripts/ide-build-script.sh
+++ scripts/ide-build-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Configuring Git ..."
 echo -n "Enter your name and press [ENTER]: "
 read name
 echo -n "Enter your email address [ENTER]: "
@@ -10,3 +11,11 @@ git config --global user.name $name
 git config --global user.email $email
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
+
+cd /tmp
+
+git clone https://github.com/MitchyBAwesome/dogs.git
+
+cp /tmp/dogs/* ~/environment/dogs
+
+cd ~/environment/dogs
